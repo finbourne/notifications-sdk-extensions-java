9e5c49770756a079dcf58d642a0a7692ff904239
Concourse <concourse@finbourne.com>
2022-05-16 08:37:30 +0000

    Update SDK Extensions 0.17.0 - autogenerated by MustacheGenerator

--- sdk/src/main/java/com/finbourne/notifications/extensions/HttpClientFactory.java
+++ sdk/src/main/java/com/finbourne/notifications/extensions/HttpClientFactory.java
@@ -5,6 +5,7 @@ import okhttp3.OkHttpClient;
 
 import java.net.InetSocketAddress;
 import java.net.Proxy;
+import java.util.concurrent.TimeUnit;
 
 /**
 * Builds http client to communicate to notifications API instances.
@@ -19,7 +20,20 @@ public class HttpClientFactory {
     * @param apiConfiguration configuration to connect to notifications API
     * @return an client for http calls to notifications API
     */
-    public OkHttpClient build(ApiConfiguration apiConfiguration){
+    public OkHttpClient build(ApiConfiguration apiConfiguration) {
+        return this.build(apiConfiguration, 10, 10);
+    }
+
+    /**
+     * Builds a {@link OkHttpClient} from a {@link ApiConfiguration} to make
+     * calls to the notifications API.
+     *
+     * @param apiConfiguration configuration to connect to notifications API
+     * @param readTimeout read timeout in seconds
+     * @param writeTimeout write timeout in seconds
+     * @return n client for http calls to notifications API
+     */
+    public OkHttpClient build(ApiConfiguration apiConfiguration, int readTimeout, int writeTimeout){
         final OkHttpClient httpClient;
 
         //  use a proxy if given
@@ -28,17 +42,22 @@ public class HttpClientFactory {
             InetSocketAddress proxy = new InetSocketAddress(apiConfiguration.getProxyAddress(), apiConfiguration.getProxyPort());
 
             httpClient = new OkHttpClient.Builder()
-                    .proxy(new Proxy(Proxy.Type.HTTP, proxy))
-                    .proxyAuthenticator((route, response) -> {
-                        String credential = Credentials.basic(apiConfiguration.getProxyUsername(), apiConfiguration.getProxyPassword());
-                        return response.request().newBuilder()
-                                .header("Proxy-Authorization", credential)
-                                .build();
+                .readTimeout(readTimeout, TimeUnit.SECONDS)
+                .writeTimeout(writeTimeout, TimeUnit.SECONDS)
+                .proxy(new Proxy(Proxy.Type.HTTP, proxy))
+                .proxyAuthenticator((route, response) -> {
+                    String credential = Credentials.basic(apiConfiguration.getProxyUsername(), apiConfiguration.getProxyPassword());
+                    return response.request().newBuilder()
+                            .header("Proxy-Authorization", credential)
+                            .build();
             })
             .build();
         }
         else {
-            httpClient = new OkHttpClient();
+            httpClient = new OkHttpClient.Builder()
+                .readTimeout(readTimeout, TimeUnit.SECONDS)
+                .writeTimeout(writeTimeout, TimeUnit.SECONDS)
+                .build();
         }
         return httpClient;
     }
